89688f39c50d2750a1ad7ebd46860a9289fda6d5
lumusitech <lumusitech@gmail.com>
2023-05-04 13:01:17 -0300

    feat: add BrowerRouter in App

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import HeroesApp from './HeroesApp'
 
 import './styles.css'
+import { BrowserRouter } from '../node_modules/react-router-dom/dist/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HeroesApp />
+    <BrowserRouter>
+      <HeroesApp />
+    </BrowserRouter>
   </React.StrictMode>,
 )
