defdc3ff3ec098db6900a5c48f8287a28d07aeee
lumusitech <lumusitech@gmail.com>
2023-05-08 00:10:55 -0300

    build: new build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://karluxio.github.io/frontend-react-heroes-spa/',
+  base: '/frontend-react-heroes-spa/',
   plugins: [react()],
 })
