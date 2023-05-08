47372b56ded8e6d53e756bfa3d7679b4c75de67e
lumusitech <lumusitech@gmail.com>
2023-05-08 14:09:03 -0300

    test: test PublicRoute.jsx

---
+++ PublicRoute.test.jsx
@@ -0,0 +1,66 @@
+import { render, screen } from '@testing-library/react'
+import { PublicRoute } from './src/router/PublicRoute'
+import { renderHook } from '@testing-library/react'
+import { useContext } from 'react'
+import { AuthContext } from './src/auth'
+import { MemoryRouter } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+
+describe('PublicRoute', () => {
+  it("should render children if not logged", () => {
+
+    const contextValue = {
+      logged: false
+    }
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <PublicRoute>
+          <h1>public route</h1>
+        </PublicRoute>
+      </AuthContext.Provider>
+    )
+
+    expect(screen.getByText("public route")).toBeTruthy()
+  })
+
+  it("should render main page if logged", () => {
+
+    const contextValue = {
+      logged: true,
+      user: {
+        name: "lux",
+        id: 123
+      }
+    }
+
+    render(
+      <AuthContext.Provider value={contextValue}>
+        <MemoryRouter initialEntries={["/login"]}>
+
+          <Routes>
+
+            <Route
+              path='/login'
+              element={
+                <PublicRoute>
+                  <h1>public route</h1>
+                </PublicRoute>
+              }
+            />
+
+            <Route path='/' element={<h1>Marvel page</h1>} />
+
+          </Routes>
+
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    // screen.debug()
+
+    expect(screen.getByRole("heading", { level: 1, name: /marvel/i }))
+  })
+})
